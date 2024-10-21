c00bb4ecfdbb973ea9827ef0b586e340890b0d66
rajith <rajith.krishnegowda@intel.com>
2024-10-21 16:54:02 +0530

    test 1.67.1

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ setup(
         'jupyterlab',
         'numpy',
         'pandas',
-        'protobuf>=3.19.6,<4.0.0',
+        'protobuf>=4.22,<6.0.0',
         'pyzmq<=26.2.0',
         'requests>=2.32.0',
         'rich',
@@ -167,7 +167,7 @@ setup(
         'tensorboardX>=2.6',
         'tqdm',
     ],
-    setup_requires=['grpcio-tools>=1.56.2,<1.67.0'],
+    setup_requires=['grpcio-tools>=1.56.2,<1.67.1'],
     python_requires='>=3.8, <3.12',
     project_urls={
         'Bug Tracker': 'https://github.com/securefederatedai/openfl/issues',
